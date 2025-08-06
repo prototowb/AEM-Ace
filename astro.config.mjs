1be88ea91524bf90dbad6747d313e9446a98c59f
Tobias Rauer <tobias.rauer@netcentric.biz>
2025-08-06 20:35:26 +0200

    AACE-003: add dist folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,19 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 import sanity from '@sanity/astro';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static', // Static site generation
   integrations: [
     vue(), 
     tailwind(),
-    react(),
     sanity({
       projectId: 'z5tty2va',
       dataset: 'production',
-      useCdn: false, // Set to false for dev, true for production
-      // Access the Studio at /admin
-      studioBasePath: '/admin',
+      useCdn: true, // Use CDN for production builds
+      // Remove studio for static builds
+      // studioBasePath: '/admin',
     })
   ]
 });
